6ba78ff6d1de22489ed943099104de783c993d0b
Luyen Huynh <huynhdinhluyen@gmail.com>
2025-05-27 16:37:23 +0700

    fix(authorization): fix role guard and jwt auth guard

--- src/shared/guards/guard.module.ts
+++ src/shared/guards/guard.module.ts
@@ -4,10 +4,11 @@ import { PassportModule } from '@nestjs/passport'
 import { BlacklistGuard } from './blacklist.guard'
 import { JwtAuthGuard } from './jwt-auth.guard'
 import { SupabaseModule } from '../modules/supabase.module'
+import { PrismaService } from '../services/prisma.service'
 
 @Module({
   imports: [PassportModule, SupabaseModule],
-  providers: [JwtStrategy, BlacklistGuard, JwtAuthGuard],
+  providers: [JwtStrategy, BlacklistGuard, JwtAuthGuard, PrismaService],
   exports: [JwtStrategy, BlacklistGuard, JwtAuthGuard],
 })
-export class GuardModule { }
+export class GuardModule {}
