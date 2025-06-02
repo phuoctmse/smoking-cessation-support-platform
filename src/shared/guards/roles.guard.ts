30f74c8dbb173b1c517645199789ed22433a668e
Luyen Huynh <huynhdinhluyen@gmail.com>
2025-06-02 14:55:16 +0700

    refactor(blog): simplify create method and enhance role validation in RolesGuard

--- src/shared/guards/roles.guard.ts
+++ src/shared/guards/roles.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common'
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
 import { RoleNameType } from '../constants/role.constant'
 import { ROLES_KEY } from '../decorators/roles.decorator'
@@ -6,7 +6,7 @@ import { GqlExecutionContext } from '@nestjs/graphql'
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) { }
+  constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
     const requiredRoles = this.reflector.getAllAndOverride<RoleNameType[]>(ROLES_KEY, [
@@ -20,17 +20,16 @@ export class RolesGuard implements CanActivate {
 
     const ctx = GqlExecutionContext.create(context)
     const request = ctx.getContext().req
-
-    // The user should already be set by JwtAuthGuard
     const user = request.user
 
     if (!user.role) {
-      throw new UnauthorizedException('User has no role!');
+      throw new UnauthorizedException('User has no role!')
     }
 
     const matchRole = requiredRoles.some((role) => user.role === role)
+
     if (!matchRole) {
-      throw new UnauthorizedException('You do not have access to this resource');
+      throw new ForbiddenException('Access denied')
     }
 
     return true
