30518b0872e20ad45c6baee2877cc8200a6cb7dd
phuoctmse <phuoctmse184067@fpt.edu.vn>
2025-07-28 20:31:06 +0700

    hotfix(package)package: handle possible  exception membership package

--- src/routes/membership-package/dto/request/create-membership.input.ts
+++ src/routes/membership-package/dto/request/create-membership.input.ts
@@ -1,18 +1,27 @@
-import { Field, InputType, Int } from "@nestjs/graphql";
-import { CreateMembershipPackageSchema } from "../../schema/create-membership.schema";
-import { createZodDto } from "nestjs-zod";
+import { Field, InputType, Int } from '@nestjs/graphql'
+import { CreateMembershipPackageSchema } from '../../schema/create-membership.schema'
+import { createZodDto } from 'nestjs-zod'
 
 @InputType()
 export class CreateMembershipPackageInput extends createZodDto(CreateMembershipPackageSchema) {
-    @Field(() => String)
-    name: string
+  @Field(() => String)
+  name: string
 
-    @Field(() => [String])
-    description: string[]
+  @Field(() => [String])
+  description: string[]
 
-    @Field(() => Int)
-    price: number
+  @Field(() => Int)
+  price: number
 
-    @Field(() => Int)
-    duration_days: number
+  @Field(() => Int)
+  duration_days: number
+
+  @Field(() => Boolean, { defaultValue: true })
+  is_active: boolean
+
+  @Field(() => Date, { nullable: true })
+  created_at?: Date
+
+  @Field(() => Date, { nullable: true })
+  updated_at?: Date
 }
